34d09b4fe28d16a7c2313df7ab0256fc5b7908f4
A.V.9.4 <120322748+Acafac1994@users.noreply.github.com>
2022-12-17 18:54:58 +0100

    mehere schleifen in einer

---
+++ main.js
@@ -0,0 +1,27 @@
+function main () {
+
+var days = 7;
+var weeks = 7;
+
+for (let i = 1; i < weeks; i++) {
+
+    console.log("Woche:" + i);
+
+    for(let j = 1; j <= days; j++){
+    console.log("Tag:" + j);
+
+    
+    }
+    
+}
+
+
+}
+
+// Beispiel: für Jeden Bus für jeden Pasagier mach folgendes 
+
+/*
+Zeile 3-4: variable erstellt
+
+
+*/
